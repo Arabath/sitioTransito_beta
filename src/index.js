6eda6b13f927fe0604b1e796b9457c2756515e2b
arabath <axelhobrecht@hotmail.com>
2022-06-06 20:58:44 -0300

    fin de alineado

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import App from "./App";
 import Home from "./components/Home";
-// import Accordion from '../src/components/Accordion'
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Home />
-    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
